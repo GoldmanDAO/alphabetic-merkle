28b3ad99ac3bba5c920975bb80e2b1d922d4146c
HornyTofu <hot.sexy.tofu@gmail.com>
2023-08-25 20:24:56 +0200

    Improved logging

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,24 @@ use sea_orm::{Database, DatabaseConnection, ConnectOptions};
 use sea_orm::error::DbErr;
 use std::time::Duration;
 
+use tracing_subscriber::{filter, Layer, layer::SubscriberExt, Registry};
+
+pub fn init_logging() {
+  let stdout_log = tracing_subscriber::fmt::Layer::new()
+    .with_writer(std::io::stdout)
+    .pretty()
+    .with_filter(filter::LevelFilter::INFO);
+
+  let subscriber = Registry::default()
+    .with(stdout_log);
+
+  tracing::subscriber::set_global_default(subscriber)
+    .expect("setting tracing failed");
+}
 
 async fn init_database() -> Result<DatabaseConnection, DbErr> {
   let mut opt = ConnectOptions::new("postgres://postgres:postgres@localhost/test"); //TODO Use env var
-opt.max_connections(100)
+  opt.max_connections(100)
     .min_connections(5)
     .connect_timeout(Duration::from_secs(8))
     .acquire_timeout(Duration::from_secs(8))
@@ -24,7 +38,7 @@ opt.max_connections(100)
 async fn main() {
   tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
-        .with_test_writer()
+        .with_writer(std::io::stdout)
         .init();
 
   let db = init_database().await.unwrap();
